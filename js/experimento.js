21fb8e6f38372b75ea2f9042c70ae899cc124f20
Alejandra-Sa <alejandrasadaniowski@ucami.edu.ar>
2025-10-20 19:00:37 -0300

    Refactor jsPsych initialization and trial definition

--- js/experimento.js
+++ js/experimento.js
@@ -1,13 +1,10 @@
-// Inicializar jsPsych después de que las bibliotecas estén cargadas
 const jsPsych = initJsPsych({
   on_finish: () => jsPsych.data.displayData()
 });
 
-// Definir un trial simple que muestra un texto y espera una tecla
 const bienvenida = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: 'Presioná cualquier tecla para continuar'
 };
 
-// Ejecutar el experimento con el trial en el timeline
 jsPsych.run([bienvenida]);
